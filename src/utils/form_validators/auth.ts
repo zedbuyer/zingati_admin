a12afcfa6b7016f0b78190f4a3b8502c1e54b9e8
Richard Nsama <kasonde.nsama@gmail.com>
2023-11-13 05:50:01 +0200

    Added form validator to login form

--- src/utils/form_validators/auth.ts
+++ src/utils/form_validators/auth.ts
@@ -3,3 +3,8 @@ import * as yup from "yup";
 export const forgotPasswordSchema = yup.object().shape({
   email: yup.string().required("An email address is required"),
 });
+
+export const loginAuthSchema = yup.object().shape({
+  email: yup.string().email().required("An email is required"),
+  password: yup.string().required("A password is required"),
+});
