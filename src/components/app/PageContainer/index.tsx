6902b6ac714e5b59c99697094a7b14f4f801d0d9
Richard Nsama <kasonde.nsama@gmail.com>
2023-11-13 16:48:41 +0200

    Dashboard fixed with temp values

--- src/components/app/PageContainer/index.tsx
+++ src/components/app/PageContainer/index.tsx
@@ -10,7 +10,7 @@ const PageContainer = (props: PageContainerProps) => {
     <>
       <Helmet title={props.title} />
       <div className="h-screen flex-1 px-10 py-20">
-        <h1 className="font-semibold text-3xl">{props.title}</h1>
+        <h1 className="font-semibold text-3xl mb-10">{props.title}</h1>
         {props.children}
       </div>
     </>
--- src/routes/app/Dashboard.tsx
+++ src/routes/app/Dashboard.tsx
@@ -1,5 +1,30 @@
 const Dashboard = () => {
-  return <>HI</>;
+  return (
+    <div className="w-full flex flex-col">
+      <div className="stats shadow stats-horizontal">
+        <div className="stat">
+          <div className="stat-title">Total New Customers</div>
+          <div className="stat-value">0</div>
+          <div className="stat-desc">New customers today</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Total Main Products</div>
+          <div className="stat-value">0</div>
+          <div className="stat-desc">All main products</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Total Suppliers</div>
+          <div className="stat-value">0</div>
+          <div className="stat-desc">All Zingati Suppliers</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Total New Orders</div>
+          <div className="stat-value">0</div>
+          <div className="stat-desc">New orders today</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
